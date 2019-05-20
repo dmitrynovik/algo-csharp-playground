bc469295e55d151c98e0b6c8dcc7b6492831371b
Dmitry Novik <dmitry.novik@gmail.com>
2019-05-20 12:33:24 +1000

    Init

---
+++ Algo-CSharp/IsPowerOfTwoTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algo_CSharp
+{
+    public class IsNumberOfTwoTest
+    {
+        [Fact] public void Two_Is_A_Power_Of_Two() => 2.IsPowerOfTwo().Should().Be(true);
+
+        [Fact] public void One_Is_A_Power_Of_Two() => 1.IsPowerOfTwo().Should().Be(true);
+
+        [Fact] public void Four_Is_A_Power_Of_Two() => 4.IsPowerOfTwo().Should().Be(true);
+
+        [Fact] public void Five_IsNot_A_Power_Of_Two() => 5.IsPowerOfTwo().Should().Be(false);
+
+        [Fact]  public void Zero_IsNot_A_Power_Of_Two() => 0.IsPowerOfTwo().Should().Be(false);
+
+        [Fact] public void MinusTwo_IsNot_A_Power_Of_Two() => (-2).IsPowerOfTwo().Should().Be(false);
+    }
+}
---
+++ Algo-CSharp/NumberExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Algo_CSharp
+{
+    public static class NumberExtensions
+    {
+        public static bool IsPowerOfTwo(this int num) => num > 0 && (num & (num - 1)) == 0;
+    }
+}
