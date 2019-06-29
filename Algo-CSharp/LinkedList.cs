10e0e560082f0e45b73c6aaba25de399d0b45448
Dmitry Novik <dmitry.novik@gmail.com>
2019-06-30 09:59:39 +1000

    LinkedList

---
+++ Algo-CSharp/LinkedList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_CSharp
+{
+    public class LinkedList<T>
+    {
+        private class Node<T>
+        {
+            private readonly T _value;
+
+            public Node(T value) {  _value = value; }
+
+            public LinkedList<T>.Node<T> Prev { get; set; }
+            public LinkedList<T>.Node<T> Next { get; set; }
+
+            public T Value => _value;
+        }
+
+        private LinkedList<T>.Node<T> _head;
+
+        public void Insert(T val)
+        {
+            var node = new Node<T>(val);
+            if (_head != null)
+            {
+                _head.Prev = node;
+                node.Next = _head;
+            }
+            _head = node;
+        }
+
+        public void Remove(T val)
+        {
+            var p = _head;
+            while (p != null)
+            {
+                if (p.Value.Equals(val))
+                    Remove(p);
+
+                p = p.Next;
+            }
+        }
+
+        public bool Contains(T val)
+        {
+            var p = _head;
+            while (p != null)
+            {
+                if (p.Value.Equals(val))
+                    return true;
+
+                p = p.Next;
+            }
+
+            return false;
+        }
+
+        public IEnumerable<T> Items
+        {
+            get
+            {
+                var p = _head;
+                while (p != null)
+                {
+                    yield return p.Value;
+                    p = p.Next;
+                }
+            }
+        }
+
+        private void Remove(Node<T> node)
+        {
+            if (node.Prev != null)
+                node.Prev.Next = node.Next;
+            else
+                _head = node.Next;
+
+            if (node.Next != null)
+                node.Next.Prev = node.Prev;
+        }
+    }
+}
---
+++ Algo-CSharp/Tests/LinkedListTest.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algo_CSharp.Tests
+{
+    public class LinkedListTest
+    {
+        [Fact]
+        public void Insert_Empty()
+        {
+            var ll = new LinkedList<int>();
+            ll.Insert(1);
+            ll.Items.Should().Equal(1);
+        }
+
+        [Fact]
+        public void Insert_123()
+        {
+            var ll = new LinkedList<int>();
+            ll.Insert(1);
+            ll.Insert(2);
+            ll.Insert(3);
+            ll.Items.Should().Equal(3, 2, 1);
+        }
+
+        [Fact]
+        public void Insert_111()
+        {
+            var ll = new LinkedList<int>();
+            ll.Insert(1);
+            ll.Insert(1);
+            ll.Insert(1);
+            ll.Items.Should().Equal(1, 1, 1);
+        }
+
+        [Fact]
+        public void Remove_All_1()
+        {
+            var ll = new LinkedList<int>();
+            ll.Insert(1);
+            ll.Insert(1);
+            ll.Insert(1);
+            ll.Remove(1);
+            ll.Items.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Remove_Head()
+        {
+            var ll = new LinkedList<int>();
+            ll.Insert(1);
+            ll.Insert(2);
+            ll.Insert(3);
+            ll.Remove(3);
+            ll.Items.Should().Equal(2, 1);
+        }
+
+        [Fact]
+        public void Remove_1()
+        {
+            var ll = new LinkedList<int>();
+            ll.Insert(1);
+            ll.Insert(2);
+            ll.Insert(3);
+            ll.Remove(1);
+            ll.Items.Should().Equal(3, 2);
+        }
+    }
+}
